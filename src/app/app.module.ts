1f15a27590ff2789847323750350c97f34775bce
miissra <deepak29mishra@gmail.com>
2018-12-21 00:15:52 +0530

    4 App Component Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { ShppingCartComponent } from './shpping-cart/shpping-cart.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrderComponent } from './my-order/my-order.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BsNavbarComponent
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    CheckOutComponent,
+    ShppingCartComponent,
+    ShoppingCartComponent,
+    OrderSuccessComponent,
+    MyOrderComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent
   ],
   imports: [
     BrowserModule,
